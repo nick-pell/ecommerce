78075c4917c5877e4cc81d1d91b93bdd02df6a92
nick-pell <93624627+nick-pell@users.noreply.github.com>
2023-09-05 18:38:26 -0400

    Express server set up

--- server/public/src/data.js
+++ server/public/src/data.js
@@ -3,52 +3,52 @@ let menShopItemsData = [{
     name: "Supreme Crewneck",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/supreme.webp"
+    img: "../images/supreme.webp"
 },{
     id:"item2",
     name: "Essentials Hoodie",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/essentials.png"
+    img: "../images/essentials.png"
 },{
     id:"item3",
     name: "Anti Social Tee",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/antisocial.png"
+    img: "../images/antisocial.png"
 },{
     id:"item4",
     name: "Essentials Tee",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 },{
     id:"item5",
     name: "item5",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 },
 {
     id:"item6",
     name: "item6",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 },
 {
     id:"item7",
     name: "item7",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 },
 {
     id:"item8",
     name: "item8",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 }];
 
 let womenShopItemsData = [{
@@ -56,52 +56,52 @@ let womenShopItemsData = [{
     name: "wSupreme Crewneck",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/supreme.webp"
+    img: "../images/supreme.webp"
 },{
     id:"witem2",
     name: "wEssentials Hoodie",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/essentials.png"
+    img: "../images/essentials.png"
 },{
     id:"witem3",
     name: "wAnti Social Tee",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/antisocial.png"
+    img: "../images/antisocial.png"
 },{
     id:"witem4",
     name: "wEssentials Tee",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 },{
     id:"witem5",
     name: "witem5",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"witem6",
     name: "witem6",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"witem7",
     name: "witem7",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"witem8",
     name: "witem8",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 }];
 
 let kidShopItemsData = [{
@@ -109,52 +109,52 @@ let kidShopItemsData = [{
     name: "kSupreme Crewneck",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/supreme.webp"
+    img: "../images/supreme.webp"
 },{
     id:"kitem2",
     name: "kEssentials Hoodie",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/essentials.png"
+    img: "../images/essentials.png"
 },{
     id:"kitem3",
     name: "kAnti Social Tee",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/antisocial.png"
+    img: "../images/antisocial.png"
 },{
     id:"kitem4",
     name: "kEssentials Tee",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 },{
     id:"kitem5",
     name: "kitem5",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"kitem6",
     name: "kitem6",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"kitem7",
     name: "kitem7",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"kitem8",
     name: "kitem8",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 }];
 
 let accessoriesShopItemsData = [{
@@ -162,52 +162,52 @@ let accessoriesShopItemsData = [{
     name: "aSupreme Crewneck",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/supreme.webp"
+    img: "../images/supreme.webp"
 },{
     id:"aitem2",
     name: "aEssentials Hoodie",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/essentials.png"
+    img: "../images/essentials.png"
 },{
     id:"aitem3",
     name: "aAnti Social Tee",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/antisocial.png"
+    img: "../images/antisocial.png"
 },{
     id:"aitem4",
     name: "aEssentials Tee",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 },{
     id:"aitem5",
     name: "aitem5",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"aitem6",
     name: "aitem6",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"aitem7",
     name: "aitem7",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"aitem8",
     name: "aitem8",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 }];
 
 let saleShopItemsData = [{
@@ -215,52 +215,52 @@ let saleShopItemsData = [{
     name: "sSupreme Crewneck",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/supreme.webp"
+    img: "../images/supreme.webp"
 },{
     id:"sitem2",
     name: "sEssentials Hoodie",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/essentials.png"
+    img: "../images/essentials.png"
 },{
     id:"sitem3",
     name: "sAnti Social Tee",
     price: 100,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/antisocial.png"
+    img: "../images/antisocial.png"
 },{
     id:"sitem4",
     name: "sEssentials Tee",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/fogshirt.webp"
+    img: "../images/fogshirt.webp"
 },{
     id:"sitem5",
     name: "sitem5",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"sitem6",
     name: "sitem6",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"sitem7",
     name: "sitem7",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 },
 {
     id:"sitem8",
     name: "sitem8",
     price: 200,
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit?" ,
-    img: "/images/womentest.webp"
+    img: "../images/womentest.webp"
 }];
 
 let allShopItemsData = menShopItemsData.concat(womenShopItemsData).concat(kidShopItemsData).concat(accessoriesShopItemsData).concat(saleShopItemsData);
--- server/server.js
+++ server/server.js
@@ -274,6 +274,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
